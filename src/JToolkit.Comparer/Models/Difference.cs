2d1e2973a3547249f53a6a38c776c16402a5fe47
Lukas Navašinskas <Lukas.Navasinskas@adform.com>
2024-03-20 13:17:03 +0200

    difference class added

---
+++ src/JToolkit.Comparer/Models/Difference.cs
@@ -0,0 +1,8 @@
+namespace JToolkit.Comparer.Models;
+
+public class Difference
+{
+    public string Key { get; set; }
+    public string Expected { get; set; }
+    public string Actual { get; set; }
+}
